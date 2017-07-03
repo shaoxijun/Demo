2352f84a6b1673089fbbc2f4051f338a7060c403
shaoxijun <419959924@qq.com>
2017-07-03 16:51:00 +0800

    170703作业

---
+++ workspace/Demo3.java
@@ -0,0 +1,11 @@
+public class Demo3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("**********************");
+		System.out.println("ÍÛ£¡ºÜÄÑÊÜ£¡");
+		System.out.println("**********************");
+	}
+
+
+}
