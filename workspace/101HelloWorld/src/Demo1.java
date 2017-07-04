691f0a9ea6258706418c7a0053c95b222308e131
shaoxijun <419959924@qq.com>
2017-07-04 16:50:42 +0800

    170704半成品作业

---
+++ workspace/101HelloWorld/src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("tou hen tie!");
+		System.out.println("刘德华很帅。刘亦菲很漂亮");
+		System.out.println("周杰伦没我帅");
+		System.out.println("hahahah");
+		System.out.println("我在大运河边");
+
+	}
+
+}
---
+++ workspace/101HelloWorld/src/lx3.java
@@ -0,0 +1,39 @@
+
+public class lx3
+{
+	public static void main(String[] args)
+	{
+//		int num1=1;
+//		num1=num1++;
+//		int num2=1;
+//		num2=++num2;
+//		System.out.println("num1:"+num1);
+//		System.out.println("num2:"+num2);
+		
+		
+		boolean sex=true,single=true,saled=true;
+		char m='M';
+		char f='F';
+		System.out.println("性别："+m);
+		System.out.println("性别："+f);
+		System.out.println("是否单身："+single);
+		System.out.println("房屋是否卖出:"+saled);
+		
+		char z=122;
+		char space=0;
+		System.out.println(z);
+		System.out.println(space);
+		
+		long myMoney=999999999;
+		float rate=0.041f;
+		
+		System.out.println(myMoney);
+		System.out.println(rate);
+		
+		char a1='星',a2='期',a3='几';
+		System.out.print(a1);
+		System.out.print(a2);
+		System.out.print(a3);
+		
+	}
+}
