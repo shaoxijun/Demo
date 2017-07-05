bcc7afacce121b7362c6d340e7dc36ce03b23e38
shaoxijun <419959924@qq.com>
2017-07-05 09:18:45 +0800

    170705

--- workspace/102数据类型和运算/src/lx7.java
+++ workspace/102数据类型和运算/src/lx7.java
@@ -3,12 +3,16 @@ public class lx7
 {
 	public static void main(String[] args)
 	{
-		char b='b';
-		int B=(int)b;
-		B=B+13;
-		char bb;
-		bb=(char)B;
-		System.out.println(bb);
+//		char b='b';
+//		int B=(int)b;
+//		B=B+13;
+//		char bb;
+//		bb=(char)B;
+//		System.out.println(bb);
+		int s='b'+13;
+		char d=(char)s;
+		System.out.println(d);
+				
 		
 	}
 }
