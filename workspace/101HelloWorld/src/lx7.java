1755c92ef77552672c1f3427d06214e23ebc5799
shaoxijun <419959924@qq.com>
2017-07-05 09:01:55 +0800

    170705作业

---
+++ workspace/101HelloWorld/src/lx7.java
@@ -0,0 +1,11 @@
+
+public class lx7
+{
+	public static void main(String[] args)
+	{
+		char b=98;
+		char B=98+13;
+		System.out.println(B);
+		
+	}
+}
---
+++ workspace/102数据类型和运算/src/lx7.java
@@ -0,0 +1,14 @@
+
+public class lx7
+{
+	public static void main(String[] args)
+	{
+		char b='b';
+		int B=(int)b;
+		B=B+13;
+		char bb;
+		bb=(char)B;
+		System.out.println(bb);
+		
+	}
+}
