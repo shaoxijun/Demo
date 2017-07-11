c0b0f5d0e40d981010b3250f240278100eda95bf
shaoxijun <419959924@qq.com>
2017-07-11 09:14:24 +0800

    0711

--- workspace/103流程控制/src/实心菱形.java
+++ workspace/103流程控制/src/实心菱形.java
@@ -6,13 +6,44 @@ public class 实心菱形
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
+		
+//		for(int i=1;i<=5;i++)//5层...(a+1)/2
+//		{
+//			for(int j=4;j>=i;j--)
+//			{
+//				System.out.print(' ');
+//			}
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			for(int j=2;j<=i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//			
+//			
+//		}
+//		for(int i=4;i>=1;i--)//4层(a-1/2)
+//		{
+//			for(int j=4;j>=i;j--)
+//			{
+//				System.out.print(' ');
+//			}
+//			for(int j=1;j<=i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			for(int j=2;j<=i;j++)
+//			{
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
 		System.out.print("请输入需要几行菱形；");
 		int a=sc.nextInt();
-//		打印
-//	    *
-//	   **
-//	  ***
-//	 ****
 		for(int i=1;i<=(a+1)/2;i++)//5层...(a+1)/2
 		{
 			for(int j=(a+1)/2-1;j>=i;j--)
@@ -31,20 +62,6 @@ public class 实心菱形
 			
 			
 		}
-		
-		
-		
-		
-//		打印
-//		    *
-//		   **
-//		  ***
-//		 ****
-//		*****
-//		 ****
-//		  ***
-//		   **
-//		    *
 		for(int i=(a-1)/2;i>=1;i--)//4层(a-1/2)
 		{
 			for(int j=(a-1)/2;j>=i;j--)
